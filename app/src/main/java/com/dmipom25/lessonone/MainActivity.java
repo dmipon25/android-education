78dd04c7e64df0b18e13838df6001ae2667cf68f
dmipon25 <dmipon25@yandex.ru>
2019-06-16 23:14:51 +0300

    lesson_2

--- app/src/main/java/com/dmipom25/lessonone/MainActivity.java
+++ app/src/main/java/com/dmipom25/lessonone/MainActivity.java
@@ -9,33 +9,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int counter = 2147483647;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final TextView TextField = findViewById(R.id.counter);
-        final Button ButtonInc = findViewById(R.id.increment);
-        final Button ButtonDec = findViewById(R.id.decrement);
-
-        TextField.setText(String.valueOf(counter));
-
-        ButtonInc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                counter++;
-                TextField.setText(String.valueOf(counter));
-            }
-        });
-
-        ButtonDec.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                counter--;
-                TextField.setText(String.valueOf(counter));
-            }
-        });
     }
 }
